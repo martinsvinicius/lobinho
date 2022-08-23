f24d8224c7806366e5e593b92ad5869b3693d6fe
rodrigoeduardo <rodrigoow99@hotmail.com>
2022-08-23 19:50:11 -0300

    Home page finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,72 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Button, Flex, Image, Input, Link, Text } from '@chakra-ui/react'
 
 export function Home() {
   return (
-    <Flex align="center" justify="center" bgColor="purple.500" h="100vh">
-      <Text fontSize="4xl" color="white">
-        Lobinho
-      </Text>
+    <Flex h="100vh" flexDir="column" justify="center">
+      <Flex
+        py="5rem"
+        bgColor="secondary"
+        w="100%"
+        flexDir="column"
+        align="center"
+        justify="center"
+      >
+        <Image src="logo.png" alt="Lobinho" />
+
+        <Flex mt="3rem" w="100%" maxW="360px" flexDir="column">
+          <Input
+            flex="1"
+            borderRadius="15px"
+            placeholder="Seu nome"
+            p="0.5rem 0"
+            textAlign="center"
+            fontWeight="semibold"
+            variant="filled"
+            _focus={{
+              bgColor: 'white'
+            }}
+          />
+          <Flex mt="1rem">
+            <Input
+              flex="1"
+              h="100%"
+              borderRadius="15px"
+              placeholder="ID da sala"
+              textAlign="center"
+              fontSize="2rem"
+              fontWeight="semibold"
+              variant="filled"
+              _focus={{
+                bgColor: 'white'
+              }}
+            />
+            <Button
+              bgColor="primary"
+              ml="1rem"
+              p="2rem 1rem"
+              transition="filter 0.2s"
+              _hover={{
+                filter: 'brightness(1.2)'
+              }}
+            >
+              <Image src="play.svg" alt="Play" />
+            </Button>
+          </Flex>
+          <Text color="white" fontSize="1.5rem" textAlign="center" mt="0.5rem">
+            ou{' '}
+            <Link
+              textDecor="underline"
+              fontWeight="bold"
+              transition="filter 0.2s"
+              _hover={{
+                filter: 'brightness(0.9)'
+              }}
+            >
+              Crie uma sala!
+            </Link>
+          </Text>
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
