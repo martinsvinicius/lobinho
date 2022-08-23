ab6bb2f12d8861ddebaccd9de6c245477e2a28ae
martinsvinicius <vinicius.victor.sm@gmail.com>
2022-08-23 00:22:35 -0300

    initial structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from '@chakra-ui/react'
+
+export function Home() {
+  return (
+    <Flex align="center" justify="center" bgColor="purple.500" h="100vh">
+      <Text fontSize="4xl" color="white">
+        Lobinho
+      </Text>
+    </Flex>
+  )
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    purple: {
+      200: '#646DBB',
+      500: '#34348C'
+    },
+    gray: {
+      800: '#231F20'
+    },
+    blue: {
+      500: '#1D8FCF'
+    },
+    red: {
+      500: '#EE2026'
+    },
+    yellow: {
+      500: '#F9E008'
+    },
+    orange: {
+      500: '#F47821'
+    },
+    green: {
+      500: '#5EC5BB'
+    }
+  }
+})
+
+export default theme
