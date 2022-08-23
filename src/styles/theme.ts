22b66dd856d66d465c49b6a12acd6cc65ebcf608
rodrigoeduardo <rodrigoow99@hotmail.com>
2022-08-23 19:50:01 -0300

    added global styles and tokens

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,14 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: 'Inter, sans-serif',
+        bgColor: 'primary'
+      }
+    }
+  },
   colors: {
     purple: {
       200: '#646DBB',
@@ -21,8 +29,19 @@ const theme = extendTheme({
     orange: {
       500: '#F47821'
     },
-    green: {
+    cyan: {
       500: '#5EC5BB'
+    }
+  },
+  semanticTokens: {
+    colors: {
+      primary: 'purple.500',
+      secondary: 'purple.200',
+      tertiary: 'gray.800',
+      werewolf: 'red.500',
+      village: 'yellow.500',
+      monster: 'orange.500',
+      zombie: 'cyan.500'
     }
   }
 })
